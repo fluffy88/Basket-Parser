affd01b84797cbc368700023a2666b1559e62382
Seán Dunne <sean.dunne1988@gmail.com>
2015-03-07 18:00:11 +0000

    Added a new empty basket icon

--- basket_parser_dev.js
+++ basket_parser_dev.js
@@ -433,7 +433,8 @@ function popoutTextarea(text) {
 		empty_basket.setAttribute("onclick", "emptyBasket(); hideBuildBox(); return;");
 		empty_basket.setAttribute("style", "cursor: pointer;");
 		var empty_basker_icon = document.createElement("img");
-		empty_basker_icon.setAttribute("src", "http://www.fluffy88.com/images/shopping-basket-icon.png");
+		empty_basker_icon.setAttribute("src", "http://www.findicons.com/icon/download/459008/basket_empty/32/png");
+		empty_basker_icon.setAttribute("align", "middle");
 		empty_basker_icon.setAttribute("style", "margin: 5px 5px -5px auto; -moz-transform: scaleY(-1); -webkit-transform: scaleY(-1); -o-transform: scaleY(-1); transform: scaleY(-1); filter: flipv;");
 		empty_basket.appendChild(empty_basker_icon);
 		empty_basket.appendChild(document.createTextNode("Empty Bastet"));
